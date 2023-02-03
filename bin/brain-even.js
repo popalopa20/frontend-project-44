288a6d4dbf53845d24229c4a2e1acb628932c92b
popalopa20 <bystrova.polina@hexly.ru>
2023-02-03 10:16:48 +0300

    .

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import greeting from '../src/cli.js';
 import { numbers } from '../src/math.js';
 import { questionForUser, checkUsersAnswer } from '../src/dop.js';
@@ -7,8 +9,8 @@ const evenGame = () => {
   for (let i = 0; i < 3; i += 1) {
     const magicNumbers = numbers();
     const userAnswer = questionForUser(magicNumbers, 'string');
-    const correct = magicNumbers % 2 === 0 ? 'yes' : 'no';
-    if (!checkUsersAnswer(userAnswer, name, i, correct)) break;
+    const correctEven = magicNumbers % 2 === 0 ? 'yes' : 'no';
+    if (!checkUsersAnswer(userAnswer, name, i, correctEven)) break;
   }
 };
 
