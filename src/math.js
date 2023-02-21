78664b70b2a0b993e55840de88b13bbfffb9854a
popalopa20 <bystrova.polina@hexly.ru>
2023-02-21 15:04:01 +0300

    final cut

--- src/math.js
+++ src/math.js
@@ -46,12 +46,12 @@ export const pass = (str) => {
   const prog = str.split(' ');
   const indOfHidden = prog.indexOf('..');
   let result = 0;
-  const firstTwo = [parseInt(prog[0], 10), parseInt(prog[1], 10)]
-  const lastTwo = [parseInt(prog[prog.length - 3], 10), parseInt(prog[prog.length - 2], 10)]
+  const firstTwo = [parseInt(prog[0], 10), parseInt(prog[1], 10)];
+  const lastTwo = [parseInt(prog[prog.length - 3], 10), parseInt(prog[prog.length - 2], 10)];
   if (indOfHidden > 1) {
-    result = firstTwo[0] + ((firstTwo[1] - firstTwo[0]) * indOfHidden)
+    result = firstTwo[0] + ((firstTwo[1] - firstTwo[0]) * indOfHidden);
   } else {
-    result = lastTwo[1] - ((lastTwo[1] - lastTwo[0]) * indOfHidden)
+    result = lastTwo[1] - ((lastTwo[1] - lastTwo[0]) * (prog.length - indOfHidden - 2));
   }
   return `${result}`;
 }
