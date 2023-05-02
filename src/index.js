d3639f64bc9c5e5a374d5ffc60d2cf2cb9d3f478
root <root@LAPTOP-7J85VK73>
2023-05-02 14:40:07 +0300

    .

--- src/index.js
+++ src/index.js
@@ -22,3 +22,24 @@ export const checkUsersAnswer = (userAnswer, userName, i, correct) => {
   if (i === 2) type(`Congratulations, ${userName}!`);
   return true;
 };
+
+export const runEngine = (rules, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rules);
+
+  const roundsCount = 3;
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, answer] = generateRound();
+    const yourAnswer = readlineSync.question(`Question: ${question} `);
+    console.log(`Your answer: ${yourAnswer}`);
+    if (yourAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}`);
+};
